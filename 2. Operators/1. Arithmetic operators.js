7c66983b969a065ca8e4fe7d2c773c26c422f169
Nahid <mdnahidhosentuhin@gmail.com>
2025-12-03 20:03:31 +0600

    Operators folder added with Arithmetic and Assignment operators files

---
+++ 2. Operators/1. Arithmetic operators.js
@@ -0,0 +1,52 @@
+let num1 = 10;
+let num2 = 6;
+
+// 1. Addition operator (+)
+let sum = num1 + num2;
+console.log(sum);    // 16
+
+// 2. Subtraction operator (-)
+let diff = num1 - num2;
+console.log(diff);    // 4
+
+// 3. Multiplication operator (*)
+let prod = num1 * num2;
+console.log(prod);    // 60
+
+// 4. Division operator (/)
+let quo = num1 / num2;
+console.log(quo);    // 1.6666666666666667
+
+// 5. Modulus operator (%)
+let rem = num1 % num2;
+console.log(rem);    // 4
+
+// 6. Exponentiation operator (**)
+let exp = num1 ** 2;
+console.log(exp);      // 100
+
+// 7. Increment operator (++)
+num1++;
+console.log(num1);    // 11
+
+// 8. Decrement operator (--)
+num2--;
+console.log(num2);    // 5
+
+//Pre increment/decrement
+let x = 7;
+let y = 5;
+
+console.log(++x);    // 8
+console.log(--y);    // 4
+console.log(x);      // 8
+console.log(y);      // 4
+
+//Post increment/decrement
+let a = 9;
+let b = 6;
+console.log(a++);      // 9
+console.log(b--);      // 6
+
+console.log(a);        // 10
+console.log(b);        // 5
---
+++ 2. Operators/2. Assignment operators.js
@@ -0,0 +1,33 @@
+//The assignment operator assigns value to the variables.
+
+let a = 10;   // assigns value 10 to variable a
+let b = 5;    // assigns value 5 to variable b
+console.log(a);   // 10
+console.log(b);   // 5
+
+// Shorthand assignment operators combine arithmetic operations with assignment: 
+
+// 1. Addition assignment operator (+=)
+a += b;   //  a = a + b
+console.log(a);   // 15
+
+// 2. Subtraction assignment operator (-=)
+a -= b;   //  a = a - b
+console.log(a);   // 10
+
+// 3. Multiplication assignment operator (*=)
+a *= b;   //  a = a * b 
+console.log(a);   // 50
+
+// 4. Division assignment operator (/=)
+a /= b;   //  a = a / b 
+console.log(a);   // 10
+
+// 5. Modulus assignment operator (%=)
+a %= b;   //  a = a % b 
+console.log(a);   // 0
+
+// 6. Exponentiation assignment operator (**=)
+a = 2;   
+a **= b;  //  a = a ** b 
+console.log(a);   // 32 
